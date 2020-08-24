8c67a44c3b794c0f29cba755368ccda63397e013
Amauri Nogueira <50035391+amaurinogueira@users.noreply.github.com>
2020-08-24 20:18:31 -0300

    SubindoAplicacao

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '19891011a*',
+    database: 'sqlnode',
+    define:{
+        timestamps: true,
+        underscored: true,
+        
+    },
+
+};
